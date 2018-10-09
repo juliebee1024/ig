712a2ca1c4707f4c6cc75b56443075d481a7f73a
Julie Bao <juliebao@Julies-MacBook-Pro.local>
2018-10-08 22:45:15 -0700

    initial commit

---
+++ ig/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ig
+//
+//  Created by Julie Bao on 10/8/18.
+//  Copyright © 2018 Julie Bao. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
